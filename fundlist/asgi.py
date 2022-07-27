94a9eda49797cefd1528d0cba6de64ac74ab44f1
Nic <nicisme@gmail.com>
2022-07-27 11:41:23 +0200

    Name Updates

--- fundlist/asgi.py
+++ fundlist/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dason.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fundlist.settings")
 
 application = get_asgi_application()
--- fundlist/wsgi.py
+++ fundlist/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dason.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fundlist.settings')
 
 application = get_wsgi_application()
