70580c68236e4bf017028b4b14d152a15b4f1bcf
Nic <nicisme@gmail.com>
2022-07-27 18:50:04 +0200

    Merge issue static

--- fundlist/settings.py
+++ fundlist/settings.py
@@ -157,6 +157,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
