65ed337060a780f1bf505c539fc9caba1f940d59
Nic <nicisme@gmail.com>
2022-07-28 18:10:07 +0200

    Template prints funds from database

--- funds/views.py
+++ funds/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
-from .models import Fund, Company
+from .models import Fund, Company, PriceData
 
 # Create your views here.
 
 
 def get_funds(request):
-    fund_list = Fund.objects
-    company_list = Company.objects
-
+    fund_list = Fund.objects.order_by("id")
+    company_list = Company.objects.order_by("id")
+    price_list = PriceData.objects.order_by("id")
     context = {"fund_list": fund_list, "company_list": company_list}
+
     return render(request, "dashboard.html", context)
