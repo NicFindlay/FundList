8ab3ed5a44ac0f9cb6ccdf819bf1f4b8819405db
Nic <nicisme@gmail.com>
2022-08-01 20:49:22 +0200

    Second fund data error test

--- manage.py
+++ manage.py
@@ -2,10 +2,12 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from selenium import webdriver
 
 
 def main():
     """Run administrative tasks."""
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fundlist.settings")
     try:
         from django.core.management import execute_from_command_line
@@ -16,6 +18,12 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
+
+
+def extraction():
+    DRIVER_PATH = "/Users/nic/Documents/Django/FundList.co.za/chromedriver"
+    driver = webdriver.Chrome(executable_path=DRIVER_PATH)
+    driver.get("https://google.com")
 
 
 if __name__ == "__main__":
