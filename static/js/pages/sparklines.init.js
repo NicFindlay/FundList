98d8c01e001d08b6959f5573e977f58e949f86a4
Nic <nicisme@gmail.com>
2022-08-30 17:31:31 +0200

    Sparkline Charts implementation

--- static/js/pages/sparklines.init.js
+++ static/js/pages/sparklines.init.js
@@ -6,30 +6,16 @@ Contact: themesdesign.in@gmail.com
 File: Sparkline chart Init
 */
 
-// get colors array from the string
-function getChartColorsArray(chartId) {
-    var colors = $(chartId).attr('data-colors');
-    var colors = JSON.parse(colors);
-    return colors.map(function (value) {
-        var newValue = value.replace(' ', '');
-        if (newValue.indexOf('--') != -1) {
-            var color = getComputedStyle(document.documentElement).getPropertyValue(newValue);
-            if (color) return color;
-        } else {
-            return newValue;
-        }
-    })
-}
 
 $(document).ready(function () {
     var SparklineCharts = function () {
 
-        var sparkline4ChartColors = getChartColorsArray("#sparkline4");
+
         $("#sparkline4").sparkline([0, 23, 43, 35, 44, 45, 56, 37, 40, 45, 56, 7, 10], {
             type: 'line',
             width: '160',
             height: '30',
-            lineColor: sparkline4ChartColors,
+            lineColor: '#F48B00',
             fillColor: 'transparent',
             spotColor: 'transparent',
             lineWidth: 2,
