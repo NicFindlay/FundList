ad4517f802fb6bb3472b4b821ae2e139276739eb
Nic <nicisme@gmail.com>
2022-09-01 08:26:36 +0200

    Removing smtp

--- fundlist/settings.py
+++ fundlist/settings.py
@@ -176,8 +176,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp-relay.sendinblue.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "nic@visualclickstudio.com"
-EMAIL_HOST_PASSWORD = "fGYcrqydIB0gpDxZ"
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
 DEFAULT_FROM_EMAIL = "info@fundlist.co.za"
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
